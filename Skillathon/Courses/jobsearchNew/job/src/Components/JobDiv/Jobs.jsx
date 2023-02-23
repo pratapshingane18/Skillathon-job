b7aef056daeb7d9e91019af5d403d301790f2821
pratapshingane18 <sushrutrao465@gmail.com>
2023-02-23 19:30:51 +0530

    Updated Job

--- Skillathon/Courses/jobsearchNew/job/src/Components/JobDiv/Jobs.jsx
+++ Skillathon/Courses/jobsearchNew/job/src/Components/JobDiv/Jobs.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {BiTimeFive} from 'react-icons/bi'
 
 const Jobs  = () =>{
     return (
@@ -8,10 +9,27 @@ const Jobs  = () =>{
 
                 <div className="group group/item singlejob w-[250px] p-[20px] bg-white rounded[10px] hover:bg-blueColor shadow-lg shadow-greyIsh-400/700 hover:shadow-lg">
                     <span className='flex justify-between items-center gap-4'>
-                        <h1 className ='text-[16px] font-semibold text-textColor group-hover:text-white'>
+                        <h1 className ='text-[16px] font-semibold text-textColor group-hover:text-red'>
                             Web Developer
                         </h1>
+                   
+
+                    <span className="flex items-center text-[#000] gap-1">
+                        <BiTimeFive />Now
                     </span>
+
+                    </span>
+
+                    <h6 className ="text-[#000]">India</h6>
+
+            <p className ='text-[13px] text-[#959595] pt-[20px] border-t-[2px] mt-[20px] group-hover:text-[#000]'>
+                
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                 Provident, dignissimos accusamus autem minus assumenda
+                  placeat quae ratione odit dolore expedita?
+                  
+            </p>
+
                 </div>
             </div>
 
