fa9cf06ceb86a67a09985e1495662a12a411cd16
pratapshingane18 <sushrutrao465@gmail.com>
2023-02-23 19:05:26 +0530

    Updated Job

--- Skillathon/Courses/jobsearchNew/job/src/Components/JobDiv/Jobs.jsx
+++ Skillathon/Courses/jobsearchNew/job/src/Components/JobDiv/Jobs.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Jobs  = () =>{
     return (
-        <div> Jobs</div>
+        <div> 
+            
+            <div className = "jobContainer flex gap-10 justify-center flex-wrap items-center py-10">
+
+                <div className="group group/item singlejob w-[250px] p-[20px] bg-white rounded[10px] hover:bg-blueColor shadow-lg shadow-greyIsh-400/700 hover:shadow-lg">
+                    <span className='flex justify-between items-center gap-4'>
+                        <h1 className ='text-[16px] font-semibold text-textColor group-hover:text-white'>
+                            Web Developer
+                        </h1>
+                    </span>
+                </div>
+            </div>
+
+        </div>
 
     )
 }
